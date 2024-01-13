61246422f0e10129cdd38256fe8d95e524c7d951
nyk9 <eijikagimoto@gamil.com>
2024-01-13 23:07:50 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -107,6 +107,23 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+
+        <a
+          href='https://swgoh.gg/'
+          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hove:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <h2 className={`mb-3 text-2x1 font-semibold`}>
+            swgoh {' '}
+            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            swgoh.gg
+          </p>
+        </a>
       </div>
     </main>
   )
