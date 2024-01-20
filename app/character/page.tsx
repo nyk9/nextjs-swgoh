b70011f4704af786455db6bf13c596d181a89d04
nyk9 <eijikagimoto@gamil.com>
2024-01-21 00:00:24 +0900

    テキストでの検索機能の実装(名前、属性)完了

--- app/character/page.tsx
+++ app/character/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import React, { useState } from "react";
 import characters, { Characters } from "./characters";
 import "./styles.css";
-
-
 
 export default function Character(
     // prop:{
@@ -12,21 +11,37 @@ export default function Character(
     // }
 ) {
     const char: Characters[] = characters;
+    const [searchTerm, setSearchTerm] = useState<string>('');
+    const [searchResults, setSearchResults] = useState<Characters[]>(char);
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const term = e.target.value;
+        setSearchTerm(term);
+
+        const results = char.filter((item) =>
+            // item.name.toLowerCase().includes(term.toLowerCase())
+            new RegExp(term, 'i').test(item.name) || new RegExp(term, 'i').test(item.property)
+        );
+        setSearchResults(results);
+    };
     return (
         <>
-            <Link 
+            <Link
                 href={"/"}
                 className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-2 lg:dark:bg-zinc-800/30"
             >Home</Link>
+            <form>
+                <input type="text" placeholder="キャラ名" className="flexd flex mt-20" value={searchTerm} onChange={handleSearch} />
+            </form>
             <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-5 lg:text-left p-4">
-                {char.map((char) => {
+                {searchResults.map((char) => {
                     return (
                         <div className="border border-gray-300 rounded m-1" key={char.name}>
                             <h1 className="text-yellow-900">{char.name}</h1>
                             <Image alt={char.name} src={char.src}></Image>
                             <span>{char.property}</span>
                         </div>
-                    );
+                    )
+
                 })}
             </div>
         </>
