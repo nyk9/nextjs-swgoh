227aee964d50858c6eddb0f965260c8bc6bd1003
key-books <eijikagimoto@gmail.com>
2024-02-09 22:12:59 +0900

    若干の修正 commit

---
+++ app/character/[id]/page.tsx
@@ -0,0 +1,23 @@
+
+// import { Characters } from "@/app/data/characters";
+
+// export default async function characterDetail() {
+//     const res = await fetch ('characters', {
+//         next: {
+//             revalidate: 5,
+//         }
+//     });
+//     const data: Characters = await res.json();
+//     return (
+//         <div className="bg-gray-700">
+//             {data.name}
+//         </div>
+//     );
+// }
+
+export default function characterDetail() {
+    return(
+        <>
+        </>
+    );
+};
