4ead521adfa043248e3d8b184b2bbfced2aad146
key-books <eijikagimoto@gmail.com>
2024-05-28 18:52:53 +0900

    clean: typesディレクトリ内でまとめた

---
+++ types/swgoh/abilitieLists/abilitiesList.ts
@@ -0,0 +1,21 @@
+type abilitiesList = {
+    base_id: string,
+    ability_id: string,
+    name: string,
+    image: string,
+    url: string,
+    tier_max: number,
+    is_zeta: boolean,
+    is_omega: boolean,
+    is_omicron: boolean,
+    is_ultimate: boolean,
+    description: string,
+    combat_type: number,
+    omicron_node: number,
+    type: number,
+    character_base_id: string,
+    ship_base_id: string | null,
+    omicron_battle_types: string[],
+};
+
+export default abilitiesList;
