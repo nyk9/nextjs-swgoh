7d01d66d79d642a17a45704ddfbb40c61fd69c32
key-books <eijikagimoto@gmail.com>
2024-06-25 22:50:51 +0900

    add: supabaseで管理するtableの型を定義

---
+++ types/counters/table.ts
@@ -0,0 +1,9 @@
+export type CounterTable = {
+    id: number;
+    user_name: string;
+    allied_leader: string;
+    allied_characters?: string[];
+    enemy_leader: string;
+    enemy_characters?: string[];
+    description?: string;
+}
