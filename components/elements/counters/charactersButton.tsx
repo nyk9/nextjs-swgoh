c98dcd5e4b989b60905cbea138157831a381d5a9
nyk9 <eijikagimoto@gamil.com>
2024-06-26 10:56:06 +0900

    update: フォームの雛形が完成した

--- components/elements/counters/charactersButton.tsx
+++ components/elements/counters/charactersButton.tsx
@@ -3,17 +3,26 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigge
 import Image from "next/image";
 import { Dispatch, SetStateAction } from "react";
 
-export const CharactersButton = (props: {
-    characters: string,
-    addCharacters: Dispatch<SetStateAction<string>>
+export const CharacterButton = (params: {
+    characters: string[],
+    index: number,
+    addCharacter: Dispatch<SetStateAction<string[]>>
 }) => {
     return (
         <>
             <DropdownMenu>
-                <DropdownMenuTrigger className="rounded-full bg-neutral-600">{props.characters}</DropdownMenuTrigger>
+                <DropdownMenuTrigger className="rounded-full bg-neutral-600">{params.characters[params.index]}</DropdownMenuTrigger>
                 <DropdownMenuContent className="h-80 overflow-y-auto">
                     {characters.map((char) => (
-                        <DropdownMenuItem key={char.name} className="bg-teal-800" onClick={(e) => props.addCharacters(char.name)}>
+                        <DropdownMenuItem
+                            key={char.name}
+                            className="bg-teal-800"
+                            onClick={() => {
+                                const characters = [...params.characters];
+                                characters[params.index] = char.name;
+                                params.addCharacter(characters);
+                            }}
+                        >
                             <Image src={char.src} alt={char.name} width={50} height={50} />
                         </DropdownMenuItem>
                     ))}
