f20fb5013e88c0cc22c74bfe2bc81f75434ae85c
key-books <eijikagimoto@gmail.com>
2024-07-09 16:26:03 +0900

    clean

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Footer } from "@/components/layouts/footer/Footer"
+import { Footer } from "@/components/layouts/footer/fotter"
 import { Header } from "@/components/layouts/header/Header"
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
