2c06b4e71bd943e22f1a1f548746ea59a731ff77
key-books <eijikagimoto@gmail.com>
2024-10-31 23:42:33 +0900

    style: フッターのファイル名を正しいものに変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Footer } from "@/components/layouts/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
